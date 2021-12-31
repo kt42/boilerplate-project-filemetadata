32e15bf5ac4252c6ff1dd6521c3b53c2b5c37df4
Kevin Teeling <kteeling42@gmail.com>
2021-12-31 10:23:08 +0000

    finished

--- server.js
+++ server.js
@@ -7,8 +7,25 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
+   // req.file is the name of your file in the form above, here 'uploaded_file'
+   // req.body will hold the text fields, if there were any 
+   console.log(req.file, req.body)
+
+   var retObj = {"name": req.file.originalname, "type":req.file.mimetype, "size":req.file.size};
+   
+   res.json(retObj)
 });
 
 
@@ -16,5 +33,5 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port  http://localhost:' + port)
 });
